b724d92e25ebde405045ce3cd6182d03b08879bc
RAN WEI <ranranw1023@gmail.com>
2016-02-17 09:14:14 +0800

    Create AdventureWorks.sql

---
+++ AdventureWorks.sql
@@ -0,0 +1,19 @@
+-- CustomerAW(CustomerID, FirstName, MiddleName, LastName, CompanyName, EmailAddress)
+
+-- CustomerAddress(CustomerID, AddressID, AddressType)
+
+-- Address(AddressID, AddressLine1, AddressLine2, City, StateProvince, CountyRegion, PostalCode)
+
+-- SalesOrderHeader(SalesOrderID, RevisionNumber, OrderDate, CustomerID, BillToAddressID, ShipToAddressID, ShipMethod, SubTotal, TaxAmt, Freight)
+
+-- SalesOrderDetail(SalesOrderID, SalesOrderDetailID, OrderQty, ProductID, UnitPrice, UnitPriceDiscount)
+
+-- ProductAW(ProductID, Name, Color, ListPrice, Size, Weight, ProductModelID, ProductCategoryID)
+
+-- ProductModel(ProductModelID, Name)
+
+-- ProductCategory(ProductCategoryID, ParentProductCategoryID Name)
+
+-- ProductModelProductDescription(ProductModelID, ProductDescriptionID, Culture)
+
+-- ProductDescription(ProductDescriptionID, Description)
