74ae76b447ad0e0a2287981801fb165b6085c531
RAN WEI <ranranw1023@gmail.com>
2016-04-24 17:53:35 -0400

    Update AdventureWorks.sql

--- AdventureWorks.sql
+++ AdventureWorks.sql
@@ -57,5 +57,39 @@ ORDER BY c.CompanyName = 'Riding Cycles';
 
 -- #7. A "Single Item Order" is a customer order where only one item is ordered. 
 -- Show the SalesOrderID and the UnitPrice for every Single Item Order.
+SELECT s.SalesOrderID, s.UnitPrice
+FROM ProductAW p JOIN SalesOrderDetail s ON p.ProductID = s.ProductID
+GROUP BY s.SalesOrderID
+HAVING count(p.ProductID) = 1;
+
+-- #8. Where did the racing socks go? 
+-- List the product name and the CompanyName for all Customers who ordered ProductModel 'Racing Socks'.
+
+SELECT p1.Name, c1.CompanyName
+FROM CustomerAW c1
+JOIN SalesOrderHeader s2 ON c1.CustomerID = s2.CustomerID
+JOIN SalesOrderDetail s1 ON s1.SalesOrderID = s2.SalesOrderID
+JOIN ProductAW p1 ON s1.ProductID = p1.ProductID
+JOIN ProductModel p2 ON p1.ProductModelID = p2.ProductModelID
+WHERE p2.Name = 'Racing Socks'
+
+-- #9. Show the product description for culture 'fr' for product with ProductID 736.
+SELECT p1.Description
+FROM ProductDescription p1 
+JOIN ProductModelProductDescription p2 ON p1.ProductDescriptionID = p2.ProductDescriptionID
+JOIN ProductModel p3 ON p2.ProductModelID = p3.ProductModelID
+JOIN ProductAW p4 ON p4.ProductModelID = p3.ProductModelID
+WHERE p4.ProductID = 736 and p2.Culture = 'fr'
+
+-- #10. Use the SubTotal value in SaleOrderHeader to list orders from the largest to the smallest. 
+-- For each order show the CompanyName and the SubTotal and the total weight of the order.
+SELECT c1.CompanyName, s2.SubTotal, p1.Weight
+FROM ProductAW p1 
+JOIN SalesOrderDetail s1 ON s1.ProductID = p1.ProductID
+JOIN SalesOrderHeader s2 ON s1.SalesOrderID = s2.SalesOrderID
+JOIN CustomerAW c1 ON s2.CustomerID = c1.CustomerID
+ORDER BY s2.SubTotal DESC;
+
+-- #11. How many products in ProductCategory 'Cranksets' have been sold to an address in 'London'?
 
 -- wo shi meme ran
